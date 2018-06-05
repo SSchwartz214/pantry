2a08819130bc35880b46ada040fec31a467e3804
Seth Schwartz <sschwartz214@gmail.com>
2018-06-05 09:24:04 -0600

    adds initialize and stock check methods

--- lib/pantry.rb
+++ lib/pantry.rb
@@ -1,3 +1,14 @@
 class Pantry
+  attr_reader :stock
+
+  def initialize
+    @stock = {}
+  end
+
+  def stock_check(item)
+    @stock.count
+  end
+
+
 
 end
--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -3,5 +3,32 @@ require 'minitest/autorun'
 require 'minitest/pride'
 
 class PantryTest < Minitest::Test
+  def test_it_exists
+    pantry = Pantry.new
+
+    assert_instance_of Pantry, pantry
+  end
+
+  def test_it_starts_with_no_stock
+    pantry = Pantry.new
+
+    assert_equal ({}), pantry.stock
+  end
+
+  def test_it_can_check_stock
+    pantry = Pantry.new
+
+    assert_equal 0, pantry.stock_check("Cheese")
+  end
+
 
 end
+
+#
+# pantry.restock("Cheese", 10)
+# pantry.stock_check("Cheese")
+# # => 10
+#
+# pantry.restock("Cheese", 20)
+# pantry.stock_check("Cheese")
+# # => 30
